1d489fd2da28568d65b3a03e8792875e82beb5fa
Stormtrooper <89123989440@mail.ru>
2021-10-20 22:43:26 +0500

    test index.php

--- public/index.php
+++ public/index.php
@@ -16,6 +16,8 @@ define('LARAVEL_START', microtime(true));
 |
 */
 
+//123213
+
 if (file_exists(__DIR__.'/../storage/framework/maintenance.php')) {
     require __DIR__.'/../storage/framework/maintenance.php';
 }
