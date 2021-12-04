02b340c6e0c242d806a50acb3401e8f6260af32a
Stormtrooper <89123989440@mail.ru>
2021-12-04 11:31:34 +0500

    правки ссылок на фото на сервере

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,1 +1,1 @@
-<img src="http://project/storage/logo/Logo.png" {{ $attributes }} style="height: 9rem; border-radius: 117px;padding: 10px 10px;margin-top: 70px;background: #ffbeb5;">
+<img src="{{asset(Storage::url('public/logo/Logo.png'))}}" {{ $attributes }} style="height: 9rem; border-radius: 117px;padding: 10px 10px;margin-top: 70px;background: #ffbeb5;">
