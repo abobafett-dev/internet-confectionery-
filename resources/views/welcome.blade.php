236bce005a7e6a145abfa29cfc0fa5f64c66a1c5
Azat <azat.abdullin72@gmail.com>
2021-12-24 20:36:59 +0500

    Хот фиксы главной страницы

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -26,42 +26,41 @@
     <div class="" style="">
         {{--    вывод продуктов с количеством по типу--}}
         @include('layouts.navigation')
-        <div class="white">
+        <div style="padding: 20px 40px;">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
 {{--                {{var_dump($productsWithTypesAndCount['торт_тест'])}}--}}
             @if(count($productsWithTypesAndCount) > 0)
-                <div >
-                    @foreach($productsWithTypesAndCount as $type => $productsOfType)
-                        <div style="">
-                            <h3 style="all:revert; margin: 0px 0px 20px 0px; text-align: center;"> Топ-5 из категории «{{$type}}»</h3>
-                            <div style="display: flex; min-width: 100%; justify-content: space-evenly; box-shadow: 0px 0px 5px #97959536; padding:20px 10px;">
-                            @for($i = 0; $i < 5; $i++)
-                                <form action="{{route('addProductInCart', ['product'=>$productsOfType[$i]['id']])}}" method="POST" style=" max-width: 13em; padding: 1em; text-align: center; border: 1px solid rgba(151,149,149,0.29); border-radius: 5px;">
-                                    <h4 style="all:revert; margin: 0px; text-align: center; margin-bottom: 10px;">{{$productsOfType[$i]->name}}</h4>
-                                    <div style="margin: auto;">
-                                        <img style="border-radius: 5px;" src="{{$productsOfType[$i]['photo']}}" alt="" style="width: 10em">
-                                    </div>
-                                    <button>Добавить в корзину</button>
-                                    {{ csrf_field() }}
-                                </form>
-                            @endfor
-                            </div>
+                @foreach($productsWithTypesAndCount as $type => $productsOfType)
+                    <div style="margin:40px 0px;">
+                        <h3 style="all:revert; margin: 0px 0px 20px 0px; text-align: center;"> Топ-@if(count($productsOfType) < 5){{count($productsOfType)}} @else{{5}}@endif из категории «{{$type}}»</h3>
+                        <div style="display: flex; min-width: 100%; justify-content: space-evenly; box-shadow: 0px 0px 5px #97959536; padding:20px 10px;">
+                        @for($i = 0; ($i < 5) && ($i < count($productsOfType)); $i++)
+                            <form action="{{route('addProductInCart', ['product'=>$productsOfType[$i]['id']])}}" method="POST" style=" max-width: 13em; padding: 1em; text-align: center; border: 1px solid rgba(151,149,149,0.29); border-radius: 5px;">
+                                <h4 style="all:revert; margin: 0px; text-align: center; margin-bottom: 10px;">{{$productsOfType[$i]->name}}</h4>
+                                <div style="margin: auto;">
+                                    <img style="border-radius: 5px; width: 11em; height: 11em;" src="{{$productsOfType[$i]['photo']}}" alt="" style="width: 10em">
+                                </div>
+                                <button>Добавить в корзину</button>
+                                {{ csrf_field() }}
+                            </form>
+                        @endfor
                         </div>
-                    @endforeach
-                </div>
+                    </div>
+                @endforeach
             @endif
             </div>
         </div>
         <div class="pink">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 @if(count($componentsWithProductTypesForConstructor) > 0)
-                    <div style="display: flex; justify-content: space-around; flex-wrap: wrap;">
+                    <div style="display: flex; justify-content: space-evenly; flex-wrap: wrap;">
                     @foreach($componentsWithProductTypesForConstructor as $product_type => $componentsWithType)
-                    <div style="border: 1px solid black; padding: 5px; margin: 10px 0px" id="{{$product_type}}" class="main_cursor_hover"><h4 style="all:revert; margin: 0px; text-align: center;">{{$product_type}}</h4></div>
+                        {{var_dump($componentsWithType)}}
+                    <div style="border: 1px solid black; padding: 5px; margin: 10px 0px" id="{{$product_type}}" class="main_cursor_hover" onclick="hiddenForm({{$product_type}})"><h4 style="all:revert; margin: 0px; text-align: center;">{{$product_type}}</h4></div>
                     @endforeach
                     </div>
                     @foreach($componentsWithProductTypesForConstructor as $product_type => $componentsWithType)
-                        <form action="{{route('addProductFromConstructor')}}" method="POST">
+                        <form action="{{route('addProductFromConstructor')}}" id="{{$product_type}}" class="formConstructor" method="POST">
                             <input type="text" value="{{$product_type}}" name="product_type" hidden>
                             <div style="margin: auto; width: 35%;">
                                 @foreach($componentsWithType as $component_type => $components)
@@ -91,39 +90,6 @@
                 @endif
             </div>
         </div>
-        {{--    вывод конструктора - по типу продукта --}}
-{{--        {{var_dump($componentsWithProductTypesForConstructor)}}--}}
-{{--        @if(count($componentsWithProductTypesForConstructor) > 0)--}}
-{{--            @foreach($componentsWithProductTypesForConstructor as $product_type => $componentsWithType)--}}
-{{--                <form action="{{route('addProductFromConstructor')}}" method="POST">--}}
-{{--                    <input type="text" value="{{$product_type}}" name="product_type" hidden>--}}
-{{--                    <div style="background-color: #f39b9b;">--}}
-{{--                        {{$product_type}}--}}
-{{--                        <div>--}}
-{{--                        {{$componentsWithType[0]['weight_min']}}--}}
-{{--                        {{$componentsWithType[0]['weight_initial']}}--}}
-{{--                        {{$componentsWithType[0]['weight_max']}}--}}
-{{--                        </div>--}}
-{{--                        @foreach($componentsWithType as $component_type => $components)--}}
-
-{{--                            <div style="background-color: #ee5656; margin-left: 10px">--}}
-{{--                                @if($component_type != "0")--}}
-{{--                                    {{$component_type}}--}}
-{{--                                    <select name="constructor_{{$components[array_key_first($components)]['id_component_type']}}" id="">--}}
-{{--                                        <option value="" selected hidden></option>--}}
-{{--                                    @foreach($components as $key => $component)--}}
-{{--                                            <option value="{{$component['id']}}">{{$component['name']}}</option>--}}
-{{--                                    @endforeach--}}
-{{--                                    </select>--}}
-{{--                                @endif--}}
-{{--                            </div>--}}
-{{--                        @endforeach--}}
-{{--                        <button>Заказать!</button>--}}
-{{--                        {{csrf_field()}}--}}
-{{--                    </div>--}}
-{{--                </form>--}}
-{{--            @endforeach--}}
-{{--        @endif--}}
     </div>
     @include('layouts.footer')
 
@@ -136,4 +102,11 @@
 {{--        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8"></div>--}}
 {{--    </div>--}}
 </body>
+<script>
+    function hiddenForm(index) {
+        for(let i = 0; i < document.getElementsByClassName('formConstructor').length; i++)
+            document.getElementsByClassName('formConstructor')[i].style.visibility = 'hidden';
+        document.getElementById(index).style.visibility = 'visible';
+    }
+</script>
 </html>
