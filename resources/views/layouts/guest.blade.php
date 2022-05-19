cf7774cb0ba9c698a7b95b9f3836201e0cd8eb74
Stormtrooper <89123989440@mail.ru>
2022-05-20 00:44:15 +0500

    корректировка http в https

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -11,13 +11,13 @@
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
         <!-- Styles -->
-        @if(\request()->getSchemeAndHttpHost() == "https")
+        @if(\request()->getScheme() == "https")
             <link rel="stylesheet" href="{{ Illuminate\Support\Facades\URL::secureAsset('css/normalize.css')}}" type="text/css">
             <link rel="stylesheet" href="{{ Illuminate\Support\Facades\URL::secureAsset('css/app.css') }}" type="text/css">
             <link rel="stylesheet" href="{{ Illuminate\Support\Facades\URL::secureAsset('css/style.css')}}" type="text/css">
         @endif
 
-        @if(config('app.env') === 'local')
+        @if(config('app.env') === 'local' || \request()->getScheme() === "http")
             <link rel="stylesheet" href="{{ Illuminate\Support\Facades\URL::asset('css/normalize.css')}}" type="text/css">
             <link rel="stylesheet" href="{{ Illuminate\Support\Facades\URL::asset('css/app.css') }}" type="text/css">
             <link rel="stylesheet" href="{{ Illuminate\Support\Facades\URL::asset('css/style.css')}}" type="text/css">
@@ -28,8 +28,12 @@
 
         <!-- Scripts -->
 
-        @if(\request()->getSchemeAndHttpHost() == "https")
+        @if(\request()->getScheme() == "https")
             <script src="{{ Illuminate\Support\Facades\URL::secureAsset('js/app.js') }}" defer></script>
+        @endif
+
+        @if(config('app.env') === 'local' || \request()->getScheme() === "http")
+            <script src="{{ Illuminate\Support\Facades\URL::asset('js/app.js') }}" defer></script>
         @endif
         <style>
             html, body {
