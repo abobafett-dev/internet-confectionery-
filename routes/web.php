3f12da6796ac96cd566a18592cb7aec94b45aafe
Stormtrooper <89123989440@mail.ru>
2022-05-20 00:10:11 +0500

    корректировка http в https

--- routes/web.php
+++ routes/web.php
@@ -14,20 +14,23 @@ use App\Http\Controllers;
 |
 */
 
-Route::GET('/', [Controllers\User\UserMainPageController::class, 'createProducts'])
-    ->name('main');
+if(config('app.env') !== 'local'){
+    Route::group(['scheme'=>'HTTPS'], function(){
 
-Route::GET('/userAgreement', [Controllers\User\UserAgreementController::class, 'create'])
-    ->name('userAgreement');
+        Route::GET('/', [Controllers\User\UserMainPageController::class, 'createProducts'])
+            ->name('main');
 
-Route::GET('/dashboard', [Controllers\User\UserProfileController::class, 'create'])
-    ->middleware(['auth'])->name('dashboard');
+        Route::GET('/userAgreement', [Controllers\User\UserAgreementController::class, 'create'])
+            ->name('userAgreement');
 
-Route::POST('/dashboard/update',[Controllers\User\UserProfileController::class, 'update'])
-    ->middleware(['auth'])->name('updateProfileUser');
+        Route::GET('/dashboard', [Controllers\User\UserProfileController::class, 'create'])
+            ->middleware(['auth'])->name('dashboard');
 
-Route::POST('/dashboard/delete',[Controllers\User\UserProfileController::class, 'delete'])
-    ->middleware(['auth'])->name('deleteProfileUser');
+        Route::POST('/dashboard/update',[Controllers\User\UserProfileController::class, 'update'])
+            ->middleware(['auth'])->name('updateProfileUser');
+
+        Route::POST('/dashboard/delete',[Controllers\User\UserProfileController::class, 'delete'])
+            ->middleware(['auth'])->name('deleteProfileUser');
 
 //Route::GET('/dashboard', [Controllers\User\UserProfileController::class, 'create'])
 //    ->middleware(['auth'])->name('dashboard');
@@ -38,65 +41,153 @@ Route::POST('/dashboard/delete',[Controllers\User\UserProfileController::class, 
 //Route::POST('/dashboard/delete',[Controllers\User\UserProfileController::class, 'delete'])
 //    ->middleware(['auth'])->name('deleteProfileUser');
 
-Route::POST('/product/addProductInCart/{product}', [Controllers\User\UserProductController::class, 'addProductInCart'])
-    ->name('addProductInCart');
+        Route::POST('/product/addProductInCart/{product}', [Controllers\User\UserProductController::class, 'addProductInCart'])
+            ->name('addProductInCart');
 
-Route::POST('/product/deleteProductInCartAjax', [Controllers\User\UserProductController::class, 'deleteProductInCartAjax'])
-    ->name('deleteProductInCartAjax');
+        Route::POST('/product/deleteProductInCartAjax', [Controllers\User\UserProductController::class, 'deleteProductInCartAjax'])
+            ->name('deleteProductInCartAjax');
 
-Route::POST('/product/addProductFromConstructor', [Controllers\User\UserProductController::class, 'addProductFromConstructor'])
-    ->name('addProductFromConstructor');
+        Route::POST('/product/addProductFromConstructor', [Controllers\User\UserProductController::class, 'addProductFromConstructor'])
+            ->name('addProductFromConstructor');
 
-Route::GET('/catalog', [Controllers\User\UserCatalogController::class, 'create'])
-    ->name('catalog');
+        Route::GET('/catalog', [Controllers\User\UserCatalogController::class, 'create'])
+            ->name('catalog');
 
-Route::GET('/cart', [Controllers\User\UserCartController::class, 'create'])
-    ->name('cart');
+        Route::GET('/cart', [Controllers\User\UserCartController::class, 'create'])
+            ->name('cart');
 
-Route::POST('/cart/intervals', [Controllers\User\UserCartController::class, 'createIntervalsAjax'])
-    ->name('cartIntervals');
+        Route::POST('/cart/intervals', [Controllers\User\UserCartController::class, 'createIntervalsAjax'])
+            ->name('cartIntervals');
 
-Route::POST('/cart/addOrderToUser', [Controllers\User\UserCartController::class, 'addOrderToUser'])
-    ->name('addOrderToUser');
+        Route::POST('/cart/addOrderToUser', [Controllers\User\UserCartController::class, 'addOrderToUser'])
+            ->name('addOrderToUser');
 
-Route::GET('/order/{order}', [Controllers\User\UserOrderPageController::class, 'create'])
-    ->middleware(['auth'])->name('order');
+        Route::GET('/order/{order}', [Controllers\User\UserOrderPageController::class, 'create'])
+            ->middleware(['auth'])->name('order');
 
-Route::GET('admin/orders', [Controllers\Admin\AdminOrdersPageController::class, 'create'])
-    ->middleware(['auth'])->name('adminOrders');
+        Route::GET('admin/orders', [Controllers\Admin\AdminOrdersPageController::class, 'create'])
+            ->middleware(['auth'])->name('adminOrders');
 
-Route::POST('admin/orders/createAjax', [Controllers\Admin\AdminOrdersPageController::class, 'createAjax'])
-    ->middleware(['auth'])->name('createAjax');
+        Route::POST('admin/orders/createAjax', [Controllers\Admin\AdminOrdersPageController::class, 'createAjax'])
+            ->middleware(['auth'])->name('createAjax');
 
-Route::POST('admin/orders/changeStatusAjax', [Controllers\Admin\AdminOrdersPageController::class, 'changeStatusAjax'])
-    ->middleware(['auth'])->name('changeStatusAjax');
+        Route::POST('admin/orders/changeStatusAjax', [Controllers\Admin\AdminOrdersPageController::class, 'changeStatusAjax'])
+            ->middleware(['auth'])->name('changeStatusAjax');
 
-Route::GET('admin/products', [Controllers\Admin\AdminProductsPageController::class, 'create'])
-    ->middleware(['auth'])->name('adminProducts');
+        Route::GET('admin/products', [Controllers\Admin\AdminProductsPageController::class, 'create'])
+            ->middleware(['auth'])->name('adminProducts');
 
-Route::POST('admin/products/changeActiveAjax', [Controllers\Admin\AdminProductsPageController::class, 'changeActiveAjax'])
-    ->middleware(['auth'])->name('changeActiveAjax');
+        Route::POST('admin/products/changeActiveAjax', [Controllers\Admin\AdminProductsPageController::class, 'changeActiveAjax'])
+            ->middleware(['auth'])->name('changeActiveAjax');
 
-Route::GET('admin/products/add', [Controllers\Admin\AdminCreateProductPageController::class, 'create'])
-    ->middleware(['auth'])->name('adminProductsAdd');
+        Route::GET('admin/products/add', [Controllers\Admin\AdminCreateProductPageController::class, 'create'])
+            ->middleware(['auth'])->name('adminProductsAdd');
 
 
-Route::POST('admin/products/add/addProduct', [Controllers\Admin\AdminCreateProductPageController::class, 'addProduct'])
-    ->middleware(['auth'])->name('adminProductsAddProduct');
+        Route::POST('admin/products/add/addProduct', [Controllers\Admin\AdminCreateProductPageController::class, 'addProduct'])
+            ->middleware(['auth'])->name('adminProductsAddProduct');
 
-Route::POST('admin/products/add/addProductType', [Controllers\Admin\AdminCreateProductPageController::class, 'addProductType'])
-    ->middleware(['auth'])->name('adminProductsAddProductType');
+        Route::POST('admin/products/add/addProductType', [Controllers\Admin\AdminCreateProductPageController::class, 'addProductType'])
+            ->middleware(['auth'])->name('adminProductsAddProductType');
 
-Route::POST('admin/products/add/addComponent', [Controllers\Admin\AdminCreateProductPageController::class, 'addComponent'])
-    ->middleware(['auth'])->name('adminProductsAddComponent');
+        Route::POST('admin/products/add/addComponent', [Controllers\Admin\AdminCreateProductPageController::class, 'addComponent'])
+            ->middleware(['auth'])->name('adminProductsAddComponent');
 
-Route::POST('admin/products/add/addComponentType', [Controllers\Admin\AdminCreateProductPageController::class, 'addComponentType'])
-    ->middleware(['auth'])->name('adminProductsAddComponentType');
+        Route::POST('admin/products/add/addComponentType', [Controllers\Admin\AdminCreateProductPageController::class, 'addComponentType'])
+            ->middleware(['auth'])->name('adminProductsAddComponentType');
 
-Route::POST('admin/products/add/addIngredient', [Controllers\Admin\AdminCreateProductPageController::class, 'addIngredient'])
-    ->middleware(['auth'])->name('adminProductsAddIngredient');
+        Route::POST('admin/products/add/addIngredient', [Controllers\Admin\AdminCreateProductPageController::class, 'addIngredient'])
+            ->middleware(['auth'])->name('adminProductsAddIngredient');
 
-Route::GET('admin/products/update/{product}', [Controllers\Admin\AdminUpdateProductPageController::class, 'create'])
-    ->middleware(['auth'])->name('adminProductsUpdate');
+        Route::GET('admin/products/update/{product}', [Controllers\Admin\AdminUpdateProductPageController::class, 'create'])
+            ->middleware(['auth'])->name('adminProductsUpdate');
+    });
+}
+else{
+    Route::GET('/', [Controllers\User\UserMainPageController::class, 'createProducts'])
+        ->name('main');
+
+    Route::GET('/userAgreement', [Controllers\User\UserAgreementController::class, 'create'])
+        ->name('userAgreement');
+
+    Route::GET('/dashboard', [Controllers\User\UserProfileController::class, 'create'])
+        ->middleware(['auth'])->name('dashboard');
+
+    Route::POST('/dashboard/update',[Controllers\User\UserProfileController::class, 'update'])
+        ->middleware(['auth'])->name('updateProfileUser');
+
+    Route::POST('/dashboard/delete',[Controllers\User\UserProfileController::class, 'delete'])
+        ->middleware(['auth'])->name('deleteProfileUser');
+
+//Route::GET('/dashboard', [Controllers\User\UserProfileController::class, 'create'])
+//    ->middleware(['auth'])->name('dashboard');
+//
+//Route::POST('/dashboard/update',[Controllers\User\UserProfileController::class, 'update'])
+//    ->middleware(['auth'])->name('updateProfileUser');
+//
+//Route::POST('/dashboard/delete',[Controllers\User\UserProfileController::class, 'delete'])
+//    ->middleware(['auth'])->name('deleteProfileUser');
+
+    Route::POST('/product/addProductInCart/{product}', [Controllers\User\UserProductController::class, 'addProductInCart'])
+        ->name('addProductInCart');
+
+    Route::POST('/product/deleteProductInCartAjax', [Controllers\User\UserProductController::class, 'deleteProductInCartAjax'])
+        ->name('deleteProductInCartAjax');
+
+    Route::POST('/product/addProductFromConstructor', [Controllers\User\UserProductController::class, 'addProductFromConstructor'])
+        ->name('addProductFromConstructor');
+
+    Route::GET('/catalog', [Controllers\User\UserCatalogController::class, 'create'])
+        ->name('catalog');
+
+    Route::GET('/cart', [Controllers\User\UserCartController::class, 'create'])
+        ->name('cart');
+
+    Route::POST('/cart/intervals', [Controllers\User\UserCartController::class, 'createIntervalsAjax'])
+        ->name('cartIntervals');
+
+    Route::POST('/cart/addOrderToUser', [Controllers\User\UserCartController::class, 'addOrderToUser'])
+        ->name('addOrderToUser');
+
+    Route::GET('/order/{order}', [Controllers\User\UserOrderPageController::class, 'create'])
+        ->middleware(['auth'])->name('order');
+
+    Route::GET('admin/orders', [Controllers\Admin\AdminOrdersPageController::class, 'create'])
+        ->middleware(['auth'])->name('adminOrders');
+
+    Route::POST('admin/orders/createAjax', [Controllers\Admin\AdminOrdersPageController::class, 'createAjax'])
+        ->middleware(['auth'])->name('createAjax');
+
+    Route::POST('admin/orders/changeStatusAjax', [Controllers\Admin\AdminOrdersPageController::class, 'changeStatusAjax'])
+        ->middleware(['auth'])->name('changeStatusAjax');
+
+    Route::GET('admin/products', [Controllers\Admin\AdminProductsPageController::class, 'create'])
+        ->middleware(['auth'])->name('adminProducts');
+
+    Route::POST('admin/products/changeActiveAjax', [Controllers\Admin\AdminProductsPageController::class, 'changeActiveAjax'])
+        ->middleware(['auth'])->name('changeActiveAjax');
+
+    Route::GET('admin/products/add', [Controllers\Admin\AdminCreateProductPageController::class, 'create'])
+        ->middleware(['auth'])->name('adminProductsAdd');
+
+
+    Route::POST('admin/products/add/addProduct', [Controllers\Admin\AdminCreateProductPageController::class, 'addProduct'])
+        ->middleware(['auth'])->name('adminProductsAddProduct');
+
+    Route::POST('admin/products/add/addProductType', [Controllers\Admin\AdminCreateProductPageController::class, 'addProductType'])
+        ->middleware(['auth'])->name('adminProductsAddProductType');
+
+    Route::POST('admin/products/add/addComponent', [Controllers\Admin\AdminCreateProductPageController::class, 'addComponent'])
+        ->middleware(['auth'])->name('adminProductsAddComponent');
+
+    Route::POST('admin/products/add/addComponentType', [Controllers\Admin\AdminCreateProductPageController::class, 'addComponentType'])
+        ->middleware(['auth'])->name('adminProductsAddComponentType');
+
+    Route::POST('admin/products/add/addIngredient', [Controllers\Admin\AdminCreateProductPageController::class, 'addIngredient'])
+        ->middleware(['auth'])->name('adminProductsAddIngredient');
+
+    Route::GET('admin/products/update/{product}', [Controllers\Admin\AdminUpdateProductPageController::class, 'create'])
+        ->middleware(['auth'])->name('adminProductsUpdate');
+}
 
 require __DIR__.'/auth.php';
